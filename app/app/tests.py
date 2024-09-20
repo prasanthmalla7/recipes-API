34d67db7a60b8870490506a0048aec634da6a818
prasanthmalla7 <prasanthmalla.2698@gmail.com>
2024-09-20 15:44:10 +0530

    updated psycopg2 in req file

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,15 +2,14 @@ from django.test import SimpleTestCase
 
 from . import calc
 
-class CalcTests(SimpleTestCase):
-    
-    def test_add(self):
-        res = calc.add(5,6)
 
-        self.assertEqual(res,11)
+class CalcTests(SimpleTestCase):
+    def test_add(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
 
     def test_sub(self):
-        res = calc.sub(10,7)
+        res = calc.sub(10, 7)
 
-        self.assertEqual(res,3)
-        
+        self.assertEqual(res, 3)
