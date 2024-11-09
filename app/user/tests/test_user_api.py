751d8b5914bb2b12a5475e7c97cfbcbdb8de474d
prasanthmalla7 <prasanthmalla.2698@gmail.com>
2024-11-09 22:17:24 +0530

    made serializer codechecks

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -37,7 +37,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
-        self.assertNotIn(payload['password'], res.data)
+        self.assertNotIn('password', res.data)
 
     def test_user_with_email_exists_error(self):
         '''test to send error when user exists with existing email'''
